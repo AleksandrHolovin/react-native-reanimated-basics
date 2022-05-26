188e92f9f3560b3784366ccee427cb337de59df0
R2D2 <aleksandrswork61@gmail.com>
2022-05-26 22:24:32 +0300

    Init project create basic animation

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { Introduction } from './src/pages/Introduction/Introduction';
+
+const App: () => Node = () => {
+
+  return (
+    <Introduction />
+  );
+};
+
+
+
+export default App;
